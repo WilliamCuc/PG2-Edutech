7bef4c6fdf6902b48c124a1782db38ffe520c66f
WilliamCuc <wcuc96@gmail.com>
2026-05-31 15:29:43 -0600

    Corrección al asignar permisos

--- users/admin.py
+++ users/admin.py
@@ -41,6 +41,22 @@ class CustomUserAdmin(BaseUserAdmin, ModelAdmin):
         ("Fechas Importantes", {"fields": ("last_login", "date_joined")}),
     )
 
+    def get_fieldsets(self, request, obj=None):
+        if obj and obj.user_type in (User.UserType.ESTUDIANTE, User.UserType.PADRE):
+            return (
+                (None, {"fields": ("username", "password")}),
+                ("Información Personal", {"fields": ("first_name", "last_name", "email")}),
+                ("Roles y Tipo", {"fields": ("user_type",)}),
+                ("Fechas Importantes", {"fields": ("last_login", "date_joined")}),
+            )
+        return super().get_fieldsets(request, obj)
+
+    def save_model(self, request, obj, form, change):
+        if obj.user_type in (User.UserType.ESTUDIANTE, User.UserType.PADRE):
+            obj.is_staff = False
+            obj.is_superuser = False
+        super().save_model(request, obj, form, change)
+
 @admin.register(Estudiante)
 class EstudianteAdmin(ModelAdmin):
     pass
